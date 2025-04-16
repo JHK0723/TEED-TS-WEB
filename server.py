73d798313fc79cf3bf021d72f8d555a4d071e978
ERROR-SIDDH <57867831+ERROR-SIDDH@users.noreply.github.com>
2025-04-16 21:14:37 +0530

    update: add MongoDB URI to .env, enhance .gitignore, and populate requirements.txt

--- server.py
+++ server.py
@@ -129,4 +129,5 @@ def interval_analytics():
 
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, host="0.0.0.0", port=5000)  # Accessible from all network interfaces
+    socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+
